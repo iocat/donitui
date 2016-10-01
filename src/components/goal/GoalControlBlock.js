d47ad7e635c54abf143a1964adbdd7b99550be3b
Thanh Ngo <felix.infinite@gmail.com>
2016-10-01 18:42:24 -0400

    goal: edit position of buttons

--- src/components/goal/GoalControlBlock.js
+++ src/components/goal/GoalControlBlock.js
@@ -3,28 +3,27 @@ import React from 'react';
 import {CardActions} from 'material-ui/Card';
 import FlatButton from 'material-ui/FlatButton';
 
-export default class GoalControlBlock extends React.Component{
-    render(){
+export default class GoalControlBlock extends React.Component {
+    render() {
         return (
-            <CardActions style={{textAlign:"center"}}>
-                    <FlatButton label="Edit" primary={true} 
-                        style={
-                            {
-                                width:"40%",
-                                display: "inline-block",
-                                margin:"auto 5%"
-                            }
-                        } />
-                    <FlatButton label="Delete" secondary={true} 
-                        style={
-                            {
-                                width:"40%",
-                                display:"inline-block",
-                                margin: "auto 5%"
-                            }
-                        }/>
-                    
-                </CardActions>
+            <CardActions style={{ textAlign: "right" }}>
+                <FlatButton label="Delete" secondary={true}
+                    style={
+                        {
+                            width: "10%",
+                            display: "inline-block",
+                            margin: "auto 1%"
+                        }
+                    }/>
+                <FlatButton label="Edit" primary={true}
+                    style={
+                        {
+                            width: "10%",
+                            display: "inline-block",
+                            margin: "auto 1%"
+                        }
+                    } />
+            </CardActions>
         )
     }
 }
